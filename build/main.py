29356bc644c9e9c186f24e52bc97abe1c956bde6
Eric Conlon <ejconlon@gmail.com>
2018-01-03 21:14:49 -0800

    fetch

--- build/main.py
+++ build/main.py
@@ -7,12 +7,20 @@ import sys
 
 
 _ROOT = os.path.dirname(os.path.dirname(__file__))
+_IDRIS_COMMANDS = set(['build', 'test', 'clean', 'execute'])
+
+
+def read_packages():
+    packages_path = os.path.join(_ROOT, 'packages.json')
+    with open(packages_path, 'r') as f:
+        return json.load(f)
 
 
 def read_package(project):
     package_path = os.path.join(_ROOT, 'internal', project, 'package.json')
     with open(package_path, 'r') as f:
         return json.load(f)
+
 
 def generate(project):
     ipkg_path = os.path.join(_ROOT, 'internal', project, '{}.ipkg'.format(project))
@@ -35,38 +43,67 @@ def system(command):
     os.system(command)
 
 
-def idris(project, command):
+def sub_idris(location, project, pkgs, executable, command):
+    project_path = os.path.join(_ROOT, location, project)
     output_path = os.path.join(_ROOT, 'output')
     bin_path = os.path.join(_ROOT, 'output', 'bin')
     project_output_path = os.path.join(output_path, 'lib', project)
     os.makedirs(bin_path, exist_ok=True)
     os.makedirs(project_output_path, exist_ok=True)
     orig_idrispath = subprocess.run(['idris', '--libdir'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
-    package = read_package(project)
     idrispaths = []
     for pkg in ['base', 'prelude']:
         idrispaths.append(os.path.join(orig_idrispath, pkg))
-    for pkg in package.get('pkgs', []):
+    for pkg in pkgs:
         idrispaths.append('../../../output/lib/{}'.format(pkg))
     idrispaths_arg = ' '.join('--idrispath {}'.format(p) for p in idrispaths)
-    idris = 'cd internal/{0} && idris --ibcsubdir ../../../output/lib/{0} {2} --{1} {0}.ipkg'.format(
-        project, command, idrispaths_arg)
+    idris = 'cd {0} && idris --ibcsubdir ../../../output/lib/{1} {3} --{2} {1}.ipkg'.format(
+        project_path, project, command, idrispaths_arg)
     system(idris)
-    if package.get('executable', False) and command == 'build':
-        os.rename('internal/{0}/{0}'.format(project), os.path.join(bin_path, project))
+    generated_bin = '{0}/{1}/{1}'.format(location, project)
+    if executable and os.path.isfile(generated_bin):
+        os.rename(generated_bin, os.path.join(bin_path, project))
 
 
-def dispatch(project):
-    project_path = os.path.join(_ROOT, project)
-    command = sys.argv[2]
+def idris(location, project, command):
+    if location == 'internal':
+        package = read_package(project)
+        sub_idris(
+            location='internal',
+            project=project,
+            pkgs=package.get('pkgs', []),
+            executable=package.get('executable', False),
+            command=command)
+    elif location == 'external':
+        raise Exception('TODO')
+    else:
+        raise Exception('Unknown location', location)
+
+
+def fetch(project, ext_package):
+    ext_path = os.path.join(_ROOT, 'external')
+    os.makedirs(ext_path, exist_ok=True)
+    project_path = os.path.join(ext_path, project)
+    if not os.path.isdir(project_path):
+        cmd = 'cd {} && git clone {} && cd {} && git checkout {}'.format(
+            ext_path, ext_package['source'], project, ext_package['tag'])
+        system(cmd)
+    else:
+        print('skipping', project)
+
+
+def dispatch(location, project, command):
+    packages = read_packages()
+    assert project in packages[location]
     if command == 'generate':
+        assert location == 'internal'
         generate(project)
-    elif command == 'build':
-        idris(project, 'build')
-    elif command == 'execute':
-        idris(project, 'execute')
-    # elif command == 'fetch':
-    #     fetch(project)
+    elif command == 'fetch':
+        assert location == 'external'
+        ext_package = packages[location][project]
+        fetch(project, ext_package)
+    elif command in _IDRIS_COMMANDS:
+        idris(location, project, command)
     else:
         raise Exception('Unknown command', command)
 
@@ -76,12 +113,10 @@ def main():
     with open(packages_path, 'r') as f:
         packages = json.load(f)
     assert 'internal' in packages
-    project = sys.argv[1]
-    if project == 'all':
-        for project in packages['internal']:
-            dispatch(project)
-    else:
-        dispatch(project)
+    location = sys.argv[1]
+    project = sys.argv[2]
+    command = sys.argv[3]
+    dispatch(location, project, command)
 
 
 if __name__ == '__main__':
