ba4bfb8b9fdb47c89f61e7d4d27fb87a29916595
Luc Tielen <luc.tielen@gmail.com>
2018-01-21 11:24:59 +0100

    Implement command to generate basic lib/exe project template

--- src/Idream/Command/New.hs
+++ src/Idream/Command/New.hs
@@ -3,11 +3,120 @@
 
 module Idream.Command.New ( startNewProject ) where
 
+
+-- Imports
+
 import Control.Monad.Reader
 import Control.Monad.Logger
-import Idream.Types (Config, PackageName, PackageType)
+import Control.Monad.Except
+import Control.Exception ( try, IOException, Exception )
+import Data.Monoid ( (<>) )
+import Data.Text ( Text )
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Directory ( createDirectory )
+import System.FilePath ( (</>) )
+import Idream.Types ( Config(..)
+                    , PackageName(..)
+                    , PackageType(..) )
+
+
+-- Data types
+
+type Directory = FilePath
+data MkProjectError = MkDirError IOException
+                    | MkFileError IOException
+                    deriving (Eq, Show)
+
+
+-- Functions
+
+gitignore, idreamJson, idrPkgSetJson, libIdr, mainIdr :: Text
+idrProjectJson :: Text -> Text
+idrPkgJson :: Text -> PackageType -> Text
+
+gitignore = ".idream-work/"
+idreamJson =
+  T.unlines [ "{"
+            , "    \"cache-dir\": \".idream-work/\","
+            , "    \"project-file\": \"idr-project.json\""
+            , "}"
+            ]
+idrPkgJson pkgName pkgType =
+  T.unlines [ "{"
+            , "    \"name\": \"" <> pkgName <> "\","
+            , "    \"sourcedir\": \"src\","
+            , if pkgType == Library
+              then "    \"executable\": false,"
+              else "    \"executable\": true,"
+            , "    \"dependencies\": []"
+            , "}"
+            ]
+idrProjectJson pkgName =
+  T.unlines [ "{"
+            , "    \"packages\": ["
+            , "        \"" <> pkgName <> "\""
+            , "    ]"
+            , "}"
+            ]
+idrPkgSetJson = "{}"
+libIdr =
+  T.unlines [ "module Lib"
+            , ""
+            , "||| Library function, to be replaced with actual code."
+            , "libFunction : String"
+            , "libFunction = \"Hello, Idris!\""
+            ]
+mainIdr =
+  T.unlines [ "module Main"
+            , ""
+            , "||| Main program, to be replaced with actual code."
+            , "main : IO ()"
+            , "main = putStrLn \"Hello, Idris!\""
+            ]
 
 
 startNewProject :: (MonadReader Config m, MonadLogger m, MonadIO m)
                 => PackageName -> PackageType -> m ()
-startNewProject _ _ = $(logDebug) "startNewProject not implemented yet!"
+startNewProject pkgName pkgType =
+  either showError return =<< (runExceptT $ startNewProject' pkgName pkgType)
+
+showError :: MonadLogger m => MkProjectError -> m ()
+showError (MkDirError e) = $(logError) ("Failed to initialize project: " <> (T.pack $ show e))
+showError (MkFileError e) = $(logError) ("Failed to initialize project: " <> (T.pack $ show e))
+
+startNewProject' :: (MonadError MkProjectError m, MonadIO m)
+                 => PackageName -> PackageType -> m ()
+startNewProject' (PackageName pkgName) pkgType = do
+  safeCreateDir projectDir
+  safeCreateDir $ relDir ".idream-work"
+  safeWriteFile gitignore $ relPath ".gitignore"
+  safeWriteFile idreamJson $ relPath ".idream.json"
+  safeWriteFile (idrProjectJson pkgName) (relPath "idr-project.json")
+  safeWriteFile idrPkgSetJson $ relPath "idr-package-set.json"
+  safeCreateDir $ relDir projectDir
+  safeWriteFile (idrPkgJson pkgName pkgType) (relPath $ projectDir </> "idr-package.json")
+  safeCreateDir (relDir $ projectDir </> "src")
+  safeWriteFile (mainContents pkgType) (relPath $ projectDir </> "src" </> mainFile pkgType)
+  where projectDir = T.unpack pkgName
+        relPath path = projectDir </> path
+        relDir = relPath
+        mainFile Library = "Lib.idr"
+        mainFile Executable = "Main.idr"
+        mainContents Library = libIdr
+        mainContents Executable = mainIdr
+
+safeCreateDir :: (MonadError MkProjectError m,
+                  MonadIO m)
+              => Directory -> m ()
+safeCreateDir dir =
+  tryAction MkDirError $ createDirectory dir
+
+safeWriteFile :: (MonadError MkProjectError m, MonadIO m)
+              => Text -> FilePath -> m ()
+safeWriteFile txt path =
+  tryAction MkFileError $ TIO.writeFile path txt
+
+tryAction :: (MonadError e' m, MonadIO m, Exception e)
+          => (e -> e') -> IO a -> m a
+tryAction f = (>>= either (throwError . f) return) <$> liftIO . try
