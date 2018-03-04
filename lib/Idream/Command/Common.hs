72fa7d052937d15e2b1516fee35744d8997b334b
Luc Tielen <luc.tielen@gmail.com>
2018-03-04 01:35:48 +0100

    Implement generate ipkg command (rough first version)

--- lib/Idream/Command/Common.hs
+++ lib/Idream/Command/Common.hs
@@ -1,30 +1,91 @@
 
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell, FlexibleContexts #-}
 
 module Idream.Command.Common ( setupBuildDir
+                             , readPkgFile
                              , tryAction
+                             , invokeCmdWithEnv
+                             , handleReadPkgErr
+                             , Command
+                             , Arg
+                             , Environment
+                             , ReadPkgErr(..)
                              ) where
 
 -- TODO move to Idream.Helpers?
 -- Imports
 
 import Control.Monad.Reader
+import Control.Monad.Logger
 import Control.Monad.Except
-import Control.Exception ( Exception, try )
-import System.Directory ( createDirectoryIfMissing )
-import Idream.Types ( Config(..), buildDir )
+import Control.Exception ( Exception, IOException, try )
+import System.Directory ( createDirectoryIfMissing, getCurrentDirectory )
+import System.FilePath ( FilePath, (</>) )
+import System.Process ( createProcess, waitForProcess, proc, env )
+import System.Exit ( ExitCode(..) )
+import Idream.Types ( Config(..), Package(..), buildDir )
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as T
+import Data.Aeson ( eitherDecode )
+import Data.Monoid ( (<>) )
 
+
+-- Data types
+
+
+-- | Type alias for command when spawning an external OS process.
+type Command = String
+
+-- | Type alias for command line arguments when spawning an external OS process.
+type Arg = String
+
+-- | Type alias for an environment to be passed to a command,
+--   expressed as a list of key value pairs.
+type Environment = [(String, String)]
+
+-- | Error type used for describing errors that can occur while reading out a package file.
+data ReadPkgErr = PkgFileNotFound IOException
+                | PkgParseErr String
+                deriving (Eq, Show)
 
 -- Functions
 
 -- | Creates a build directory in which idream will store all build artifacts.
-setupBuildDir :: (MonadReader Config m, MonadIO m) => m ()
+setupBuildDir :: ( MonadReader Config m, MonadIO m ) => m ()
 setupBuildDir = do
   buildDirectory <- asks $ buildDir . buildSettings
   liftIO $ createDirectoryIfMissing True buildDirectory
 
+-- | Reads out a package file (idr-package.json)
+readPkgFile :: ( MonadError ReadPkgErr m
+               , MonadLogger m
+               , MonadIO m )
+            => FilePath -> m Package
+readPkgFile file = do
+  pkgJSON <- tryAction PkgFileNotFound $ do
+    dir <- getCurrentDirectory
+    BSL.readFile $ dir </> file
+  either (throwError . PkgParseErr) return $ eitherDecode pkgJSON
+
 -- | Helper function for running an IO action in a monad transformer stack,
 --   while catching possible exceptions and wrapping them in a custom error type.
-tryAction :: (MonadError e' m, MonadIO m, Exception e)
+tryAction :: ( MonadError e' m, MonadIO m, Exception e )
           => (e -> e') -> IO a -> m a
 tryAction f = (>>= either (throwError . f) return) <$> liftIO . try
+
+-- | Invokes a command as a separate operating system process.
+--   Allows passing additional environment variables to the external process.
+invokeCmdWithEnv :: MonadIO m => Command -> [Arg] -> Environment -> m ExitCode
+invokeCmdWithEnv cmd cmdArgs environ =
+  liftIO $ do
+    let process = (proc cmd cmdArgs) { env = Just environ }
+    (_, _, _, procHandle) <- createProcess process
+    waitForProcess procHandle
+
+-- | Helper function for handling errors related to
+--   readout of package file.
+handleReadPkgErr :: MonadLogger m => ReadPkgErr -> m ()
+handleReadPkgErr (PkgFileNotFound err) =
+  $(logError) (T.pack $ "Failed to read package file: " <> show err <> ".")
+handleReadPkgErr (PkgParseErr err) =
+  $(logError) (T.pack $ "Failed to parse package file: " <> err <> ".")
--- lib/Idream/Command/GenerateIpkg.hs
+++ lib/Idream/Command/GenerateIpkg.hs
@@ -3,10 +3,132 @@
 
 module Idream.Command.GenerateIpkg ( generateIpkgFile ) where
 
+
+-- Imports
+
+import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.Logger
-import Idream.Types (Config)
+import Control.Exception ( IOException )
+import Idream.Types
+import Idream.Graph
+import Idream.Command.Common ( tryAction, readPkgFile
+                             , handleReadPkgErr, ReadPkgErr(..) )
+import System.FilePath ( (</>) )
+import System.Directory ( removePathForcibly, createDirectory )
+import Shelly ( shelly, silently, hasExt, cp_r, findWhen
+              , fromText, toTextIgnore )
+import Data.Text ( Text )
+import Data.Monoid ( (<>) )
+import Data.List ( intercalate )
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
+
+-- Data types
+
+type Module = FilePath
+
+data IpkgMetadata = IpkgMetadata Package [Module]
+                  deriving (Eq, Show)
+
+data GenerateIpkgErr = GLoadGraphErr GraphErr
+                     | GCopyFilesErr PackageName IOException
+                     | GFindPkgFilesErr PackageName IOException
+                     | GReadPkgFileErr ReadPkgErr
+                     | GGenerateIpkgErr PackageName IOException
+                     deriving (Eq, Show)
+
+
+-- Functions
 
 generateIpkgFile :: (MonadReader Config m, MonadLogger m, MonadIO m) => m ()
-generateIpkgFile = $(logDebug) "generateIpkgFile not implemented yet!"
+generateIpkgFile = do
+  $(logInfo) "Generating .ipkg files..."
+  workDir <- asks $ buildDir . buildSettings
+  let graphFile = workDir </> "dependency-graph.json"
+  result <- runExceptT $ do
+    graph <- withExceptT GLoadGraphErr $ loadGraphFromJSON graphFile
+    mapM_ generateIpkg graph
+  case result of
+    Left err -> handleGenerateIpkgErr err
+    Right _ -> $(logInfo) "Successfully generated .ipkg files!"
+
+generateIpkg :: ( MonadError GenerateIpkgErr m
+                , MonadReader Config m
+                , MonadLogger m
+                , MonadIO m )
+             => DepNode -> m ()
+generateIpkg node@(DepNode pkgName@(PackageName name) (ProjectName projName)) = do
+  $(logDebug) ("Generating ipkg file for package: " <> name <> ".")
+  workDir <- asks $ buildDir . buildSettings
+  let pkgSrcDir = workDir </> "src" </> T.unpack projName </> T.unpack name
+      pkgBuildDir = workDir </> "build" </> T.unpack projName </> T.unpack name
+      toFilePath = fromText . T.pack
+  tryAction (GCopyFilesErr pkgName) $ do
+    removePathForcibly pkgBuildDir
+    createDirectory pkgBuildDir
+    shelly $ silently $ cp_r (toFilePath pkgSrcDir) (toFilePath pkgBuildDir)
+  generateIpkgHelper node pkgBuildDir
+
+generateIpkgHelper :: ( MonadError GenerateIpkgErr m
+                      , MonadReader Config m
+                      , MonadLogger m
+                      , MonadIO m )
+                   => DepNode -> Directory -> m ()
+generateIpkgHelper (DepNode pkgName@(PackageName name) _) pkgDir = do
+  pkgFilePath <- asks $ pkgFile . buildSettings
+  let pkgFileName = pkgDir </> pkgFilePath
+      ipkgFile = pkgDir </> T.unpack name <> ".ipkg"
+  packageResult <- runExceptT $ readPkgFile pkgFileName
+  case packageResult of
+    Left err -> throwError $ GReadPkgFileErr err
+    Right package@(Package _ _ (SourceDir srcDir) _) -> do
+      idrisFiles <- findIdrisFiles pkgName $ pkgDir </> srcDir
+      let pkgMetadata = IpkgMetadata package idrisFiles
+          contents = ipkgMetadataToText pkgMetadata
+      tryAction (GGenerateIpkgErr pkgName) $ TIO.writeFile ipkgFile contents
+
+findIdrisFiles :: ( MonadError GenerateIpkgErr m
+                  , MonadIO m )
+               => PackageName -> Directory -> m [Module]
+findIdrisFiles pkgName dir =
+  let toFilePath = fromText . T.pack
+      fromFilePath = T.unpack . toTextIgnore
+      dir' = toFilePath dir
+      isIdrisFile = pure . hasExt ".idr"
+  in tryAction (GFindPkgFilesErr pkgName) $ do
+    idrisFiles <- shelly $ silently $ findWhen isIdrisFile dir'
+    return $ fromFilePath <$> idrisFiles
+
+ipkgMetadataToText :: IpkgMetadata -> Text
+ipkgMetadataToText (IpkgMetadata pkg modules) =
+  let (Package pkgName pkgType (SourceDir srcDir) subProjects) = pkg
+      name = unPkgName pkgName
+      mods = T.pack $ intercalate "," modules
+      deps = T.intercalate "," $ unProjName <$> subProjects
+      sourceDir = T.pack srcDir
+  in T.unlines [ "-- NOTE: This is an auto-generated file by idream. Do not edit."
+               , "package " <> name
+               , "modules = " <> mods
+               , "pkgs = " <> deps
+               , "sourcedir =" <> sourceDir
+               , if pkgType == Executable then "executable = " <> name else ""
+               , if pkgType == Executable then "main = Main" else ""
+               ]
+
+handleGenerateIpkgErr :: MonadLogger m => GenerateIpkgErr -> m ()
+handleGenerateIpkgErr (GLoadGraphErr err) = handleGraphErr err
+handleGenerateIpkgErr (GCopyFilesErr pkgName err) =
+  $(logError) ("Failed to copy files into build directory for package "
+               <> unPkgName pkgName <> ", reason: "
+               <> T.pack (show err) <> ".")
+handleGenerateIpkgErr (GFindPkgFilesErr pkgName err) =
+  $(logError) ("Failed to find Idris files for package "
+               <> unPkgName pkgName <> ", reason: "
+               <> T.pack (show err) <> ".")
+handleGenerateIpkgErr (GReadPkgFileErr err) = handleReadPkgErr err
+handleGenerateIpkgErr (GGenerateIpkgErr pkgName err) =
+  $(logError) ("Failed to generate .ipkg file for package"
+              <> unPkgName pkgName <> ", reason: "
+              <> T.pack (show err) <> ".")
