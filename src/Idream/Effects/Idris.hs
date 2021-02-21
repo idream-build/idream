3dfd0baad243597aa52050304b374364da094f63
Eric Conlon <ejconlon@gmail.com>
2021-02-21 10:01:52 -0800

    process

--- src/Idream/Effects/Idris.hs
+++ src/Idream/Effects/Idris.hs
@@ -3,12 +3,55 @@ module Idream.Effects.Idris
   , idrisGetLibDir
   ) where
 
+import qualified Data.Text as T
 import Idream.App (AppM)
+import Idream.Effects.Process (Result (..), Spec (..), procEnsureSuccess, procInvoke)
 import Idream.FilePaths (Directory)
 import Idream.Types (PackageName, ProjectName)
 
 idrisGetLibDir :: AppM Directory
-idrisGetLibDir = error "TODO: idrisGetLibDir"
+idrisGetLibDir = do
+  let spec = Spec "idris2" ["--libdir"] Nothing []
+  result <- procInvoke spec
+  procEnsureSuccess spec result
+  pure (T.unpack (T.stripEnd (resultOut result)))
 
 idrisCompile :: ProjectName -> PackageName -> AppM ()
 idrisCompile _projName _pkgName = error "TODO: idrisCompile"
+
+-- runIdris :: forall e r. LastMember (SafeIO e) r
+--          => (IdrisError -> e) -> Eff (Idris ': r) ~> Eff r
+-- runIdris f = interpretM g where
+--   g :: Idris ~> SafeIO e
+--   g IdrisGetLibDir = do
+--     let eh1 = f . IdrGetLibDirErr
+--         eh2 ec err = f $ IdrInvokeErr "--libdir" [] ec err
+--         idrisArgs = [ "--libdir" ]
+--         toDir = filter (/= '\n')
+--     toDir <$> invokeIdrisWithEnv eh1 eh2 idrisArgs Nothing []
+--   g IdrisGetDocsDir = do
+--     let eh1 = f . IdrGetDocsDirErr
+--         eh2 ec err = f $ IdrInvokeErr "--docdir" [] ec err
+--         idrisArgs = [ "--docdir" ]
+--         toDir = filter (/= '\n')
+--     toDir <$> invokeIdrisWithEnv eh1 eh2 idrisArgs Nothing []
+--   g (IdrisCompile projName pkgName) = do
+--     let eh1 = IdrCompileErr projName pkgName
+--         idrisCompileArgs = [ "--verbose", "--build"]
+--         idrisInstallArgs = [ "--verbose", "--install"]
+--     void $ invokeIdrisForPkg f eh1 projName pkgName idrisCompileArgs "compile"
+--     void $ invokeIdrisForPkg f eh1 projName pkgName idrisInstallArgs "install"
+--   g (IdrisMkDocs projName pkgName) = do
+--     let idrisMkDocsArgs = ["--verbose", "--mkdoc"]
+--         idrisInstallDocsArgs = ["--verbose", "--installdoc"]
+--         eh1 = IdrMkDocsErr projName pkgName
+--     void $ invokeIdrisForPkg f eh1 projName pkgName idrisMkDocsArgs "mkdoc"
+--     void $ invokeIdrisForPkg f eh1 projName pkgName idrisInstallDocsArgs "installdoc"
+--   g (IdrisRepl projName pkgName) = do
+--     let idrisReplArgs = [ "--verbose", "--repl"]
+--         eh1 = IdrReplErr projName pkgName
+--     void $ invokeIdrisForPkg f eh1 projName pkgName idrisReplArgs "repl"
+
+-- -- | Converts a relative path into an absolute path.
+-- absPath :: (IOException -> e) -> FilePath -> SafeIO e FilePath
+-- absPath f path = liftSafeIO f $ makeAbsolute path
---
+++ src/Idream/Effects/Process.hs
@@ -0,0 +1,83 @@
+module Idream.Effects.Process
+  ( Command
+  , Arg
+  , Environment
+  , Spec (..)
+  , Result (..)
+  , procInvoke
+  , procEnsureSuccess
+  ) where
+
+import Control.Exception (Exception (..))
+import Control.Monad.IO.Class (liftIO)
+import Data.Maybe (fromJust)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Idream.App (AppM)
+import Idream.FilePaths (Directory)
+import System.Exit (ExitCode (..))
+import UnliftIO.Environment (getEnv)
+import UnliftIO.Exception (throwIO)
+import UnliftIO.Process (StdStream (CreatePipe), createProcess, cwd, env, proc, std_err, std_out, waitForProcess)
+
+-- | Type alias for command when spawning an external OS process.
+type Command = String
+
+-- | Type alias for command line arguments when spawning an external OS process.
+type Arg = String
+
+-- | Type alias for an environment to be passed to a command,
+--   expressed as a list of key value pairs.
+type Environment = [(String, String)]
+
+data Spec = Spec
+  { specCmd :: Command
+  , specCmdArgs :: [Arg]
+  , specWorkDir :: Maybe Directory
+  , specEnviron :: Environment
+  } deriving (Show, Eq)
+
+data Result = Result
+  { resultCode :: ExitCode
+  , resultOut :: Text
+  , resultErr :: Text
+  } deriving (Show, Eq)
+
+data ProcessFailedErr = ProcessFailedErr Spec Int Text Text
+  deriving (Show, Eq)
+
+instance Exception ProcessFailedErr where
+  displayException (ProcessFailedErr (Spec cmd cmdArgs maybeWorkDir _) failCode out err) =
+    concat
+      [ "Command exited with code "
+      , show failCode
+      , ": [ "
+      , unwords (cmd:cmdArgs)
+      , " ]"
+      , maybe "" (" in " <>) maybeWorkDir
+      , "\nStdout:\n"
+      , T.unpack out
+      , "\nStderr:\n"
+      , T.unpack err
+      ]
+
+-- | Invokes a command as a separate operating system process.
+--   Allows passing additional environment variables to the external process.
+procInvoke :: Spec -> AppM Result
+procInvoke (Spec cmd cmdArgs maybeWorkDir environ) = do
+  homeDir <- getEnv "HOME"
+  let environ' = ("HOME", homeDir) : environ
+      process = (proc cmd cmdArgs) { cwd = maybeWorkDir, env = Just environ'
+                                    , std_out = CreatePipe, std_err = CreatePipe }
+  (_, stdOut, stdErr, procHandle) <- createProcess process
+  code <- waitForProcess procHandle
+  out <- liftIO (TIO.hGetContents (fromJust stdOut))
+  err <- liftIO (TIO.hGetContents (fromJust stdErr))
+  pure (Result code out err)
+
+procEnsureSuccess :: Spec -> Result -> AppM ()
+procEnsureSuccess spec (Result code out err) =
+  case code of
+    ExitSuccess -> pure ()
+    ExitFailure failCode -> throwIO (ProcessFailedErr spec failCode out err)
